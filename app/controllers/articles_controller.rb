44e8cb065e55c1c3e1729396b22b372fb81fa466
Rob Ranf <rob@emiyaconsulting.com>
2024-01-29 18:38:22 -0800

    validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,11 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
 
 
